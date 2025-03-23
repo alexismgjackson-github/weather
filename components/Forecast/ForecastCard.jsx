ef38322363ab4e11b94ab5097b987591e2478b7c
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-03-23 16:33:27 -0400

    many changes

--- components/Forecast/ForecastCard.jsx
+++ components/Forecast/ForecastCard.jsx
@@ -1,6 +1,115 @@
 import "./ForecastCard.css";
+import { useState } from "react";
 
 export default function ForecastCard(props) {
+  // const [url, setUrl] = useState("");
+  const fetchedText = props.forecast.text;
+
+  const getWeatherIcon = (text) => {
+    switch (text) {
+      case "Sunny":
+        return "./assets/icons/sunny.svg";
+      case "Partly cloudy":
+        return "./assets/icons/cloudy.svg";
+      case "Cloudy":
+        return "./assets/icons/cloudy.svg";
+      case "Overcast":
+        return "./assets/icons/cloudy.svg";
+      case "Mist":
+        return "./assets/icons/misty.svg";
+      case "Patchy rain possible":
+        return "./assets/icons/rainy.svg";
+      case "Patchy snow possible":
+        return "./assets/icons/snowy.svg";
+      case "Patchy sleet possible":
+        return "./assets/icons/sleet.svg";
+      case "Patchy freezing drizzle possible":
+        return "./assets/icons/rainy.svg";
+      case "Thundery outbreaks possible":
+        return "./assets/icons/storm.svg";
+      case "Blowing snow":
+        return "./assets/icons/snowy.svg";
+      case "Blizzard":
+        return "./assets/icons/snowy.svg";
+      case "Fog":
+        return "./assets/icons/foggy.svg";
+      case "Freezing fog":
+        return "./assets/icons/foggy.svg";
+      case "Patchy light drizzle":
+        return "./assets/icons/rainy.svg";
+      case "Light drizzle":
+        return "./assets/icons/rainy.svg";
+      case "Freezing drizzle":
+        return "./assets/icons/rainy.svg";
+      case "Heavy freezing drizzle":
+        return "./assets/icons/rainy.svg";
+      case "Patchy light rain":
+        return "./assets/icons/rainy.svg";
+      case "Light rain":
+        return "./assets/icons/rainy.svg";
+      case "Moderate rain at times":
+        return "./assets/icons/rainy.svg";
+      case "Moderate rain":
+        return "./assets/icons/rainy.svg";
+      case "Heavy rain at times":
+        return "./assets/icons/rainy.svg";
+      case "Heavy rain":
+        return "./assets/icons/rainy.svg";
+      case "Light freezing rain":
+        return "./assets/icons/rainy.svg";
+      case "Moderate or heavy freezing rain":
+        return "./assets/icons/rainy.svg";
+      case "Light sleet":
+        return "./assets/icons/rainy.svg";
+      case "Moderate or heavy sleet":
+        return "./assets/icons/sleet.svg";
+      case "Patchy light snow":
+        return "./assets/icons/snowy.svg";
+      case "Light snow":
+        return "./assets/icons/snowy.svg";
+      case "Patchy moderate snow":
+        return "./assets/icons/snowy.svg";
+      case "Moderate snow":
+        return "./assets/icons/snowy.svg";
+      case "Patchy heavy snow":
+        return "./assets/icons/snowy.svg";
+      case "Heavy snow":
+        return "./assets/icons/rainy.svg";
+      case "Ice pellets":
+        return "./assets/icons/pellets.svg";
+      case "Light rain shower":
+        return "./assets/icons/rain.svg";
+      case "Moderate or heavy rain shower":
+        return "./assets/icons/rainy.svg";
+      case "Torrential rain shower":
+        return "./assets/icons/storm.svg";
+      case "Light sleet showers":
+        return "./assets/icons/snowy.svg";
+      case "Moderate or heavy sleet showers":
+        return "./assets/icons/snowy.svg";
+      case "Light snow showers":
+        return "./assets/icons/snowy.svg";
+      case "Moderate or heavy snow showers":
+        return "./assets/icons/snowy.svg";
+      case "Light showers of ice pellets":
+        return "./assets/icons/pellets.svg";
+      case "Moderate or heavy showers of ice pellets":
+        return "./assets/icons/pellets.svg";
+      case "Patchy light rain with thunder":
+        return "./assets/icons/storm.svg";
+      case "Moderate or heavy rain with thunder":
+        return "./assets/icons/snowy.svg";
+      case "Moderate or heavy sleet showers":
+        return "./assets/icons/sleet.svg";
+      case "Patchy light snow with thunder":
+        return "./assets/icons/snowy.svg";
+      case "Moderate or heavy snow with thunder":
+        return "./assets/icons/snowy.svg";
+      default:
+        return "";
+    }
+  };
+
   return (
     <>
       <li className="forecast-card">
@@ -26,7 +135,7 @@ export default function ForecastCard(props) {
           </div>
           <span className="forecast-condition">
             <img
-              src="/assets/icons/sunny.svg"
+              src={getWeatherIcon(fetchedText)}
               alt="Current weather icon"
               className="forecast-condition-icon"
             />
--- src/iconData.js
+++ src/iconData.js
@@ -1,5 +1,5 @@
-function getWeatherIcon(text) {
-  switch (id) {
+export default function getWeatherIcon(text) {
+  switch (text) {
     case "Sunny":
       return "./assets/icons/sunny.svg";
     case "Partly cloudy":
@@ -14,304 +14,91 @@ function getWeatherIcon(text) {
       return "./assets/icons/rainy.svg";
     case "Patchy snow possible":
       return "./assets/icons/snowy.svg";
+    case "Patchy sleet possible":
+      return "./assets/icons/sleet.svg";
+    case "Patchy freezing drizzle possible":
+      return "./assets/icons/rainy.svg";
+    case "Thundery outbreaks possible":
+      return "./assets/icons/storm.svg";
+    case "Blowing snow":
+      return "./assets/icons/snowy.svg";
+    case "Blizzard":
+      return "./assets/icons/snowy.svg";
+    case "Fog":
+      return "./assets/icons/foggy.svg";
+    case "Freezing fog":
+      return "./assets/icons/foggy.svg";
+    case "Patchy light drizzle":
+      return "./assets/icons/rainy.svg";
+    case "Light drizzle":
+      return "./assets/icons/rainy.svg";
+    case "Freezing drizzle":
+      return "./assets/icons/rainy.svg";
+    case "Heavy freezing drizzle":
+      return "./assets/icons/rainy.svg";
+    case "Patchy light rain":
+      return "./assets/icons/rainy.svg";
+    case "Light rain":
+      return "./assets/icons/rainy.svg";
+    case "Moderate rain at times":
+      return "./assets/icons/rainy.svg";
+    case "Moderate rain":
+      return "./assets/icons/rainy.svg";
+    case "Heavy rain at times":
+      return "./assets/icons/rainy.svg";
+    case "Heavy rain":
+      return "./assets/icons/rainy.svg";
+    case "Light freezing rain":
+      return "./assets/icons/rainy.svg";
+    case "Moderate or heavy freezing rain":
+      return "./assets/icons/rainy.svg";
+    case "Light sleet":
+      return "./assets/icons/rainy.svg";
+    case "Moderate or heavy sleet":
+      return "./assets/icons/sleet.svg";
+    case "Patchy light snow":
+      return "./assets/icons/snowy.svg";
+    case "Light snow":
+      return "./assets/icons/snowy.svg";
+    case "Patchy moderate snow":
+      return "./assets/icons/snowy.svg";
+    case "Moderate snow":
+      return "./assets/icons/snowy.svg";
+    case "Patchy heavy snow":
+      return "./assets/icons/snowy.svg";
+    case "Heavy snow":
+      return "./assets/icons/rainy.svg";
+    case "Ice pellets":
+      return "./assets/icons/pellets.svg";
+    case "Light rain shower":
+      return "./assets/icons/rain.svg";
+    case "Moderate or heavy rain shower":
+      return "./assets/icons/rainy.svg";
+    case "Torrential rain shower":
+      return "./assets/icons/storm.svg";
+    case "Light sleet showers":
+      return "./assets/icons/snowy.svg";
+    case "Moderate or heavy sleet showers":
+      return "./assets/icons/snowy.svg";
+    case "Light snow showers":
+      return "./assets/icons/snowy.svg";
+    case "Moderate or heavy snow showers":
+      return "./assets/icons/snowy.svg";
+    case "Light showers of ice pellets":
+      return "./assets/icons/pellets.svg";
+    case "Moderate or heavy showers of ice pellets":
+      return "./assets/icons/pellets.svg";
+    case "Patchy light rain with thunder":
+      return "./assets/icons/storm.svg";
+    case "Moderate or heavy rain with thunder":
+      return "./assets/icons/snowy.svg";
+    case "Moderate or heavy sleet showers":
+      return "./assets/icons/sleet.svg";
+    case "Patchy light snow with thunder":
+      return "./assets/icons/snowy.svg";
+    case "Moderate or heavy snow with thunder":
+      return "./assets/icons/snowy.svg";
     default:
       return null;
   }
 }
-
-export { getMovieGenreName };
-
-/*
-
-[
-  {
-    code: 1000,
-    day: "Sunny",
-    night: "Clear",
-    icon: 113,
-  },
-  {
-    code: 1003,
-    day: "Partly cloudy",
-    night: "Partly cloudy",
-    icon: 116,
-  },
-  {
-    code: 1006,
-    day: "Cloudy",
-    night: "Cloudy",
-    icon: 119,
-  },
-  {
-    code: 1009,
-    day: "Overcast",
-    night: "Overcast",
-    icon: 122,
-  },
-  {
-    code: 1030,
-    day: "Mist",
-    night: "Mist",
-    icon: 143,
-  },
-  {
-    code: 1063,
-    day: "Patchy rain possible",
-    night: "Patchy rain possible",
-    icon: 176,
-  },
-  {
-    code: 1066,
-    day: "Patchy snow possible",
-    night: "Patchy snow possible",
-    icon: 179,
-  },
-  {
-    code: 1069,
-    day: "Patchy sleet possible",
-    night: "Patchy sleet possible",
-    icon: 182,
-  },
-  {
-    code: 1072,
-    day: "Patchy freezing drizzle possible",
-    night: "Patchy freezing drizzle possible",
-    icon: 185,
-  },
-  {
-    code: 1087,
-    day: "Thundery outbreaks possible",
-    night: "Thundery outbreaks possible",
-    icon: 200,
-  },
-  {
-    code: 1114,
-    day: "Blowing snow",
-    night: "Blowing snow",
-    icon: 227,
-  },
-  {
-    code: 1117,
-    day: "Blizzard",
-    night: "Blizzard",
-    icon: 230,
-  },
-  {
-    code: 1135,
-    day: "Fog",
-    night: "Fog",
-    icon: 248,
-  },
-  {
-    code: 1147,
-    day: "Freezing fog",
-    night: "Freezing fog",
-    icon: 260,
-  },
-  {
-    code: 1150,
-    day: "Patchy light drizzle",
-    night: "Patchy light drizzle",
-    icon: 263,
-  },
-  {
-    code: 1153,
-    day: "Light drizzle",
-    night: "Light drizzle",
-    icon: 266,
-  },
-  {
-    code: 1168,
-    day: "Freezing drizzle",
-    night: "Freezing drizzle",
-    icon: 281,
-  },
-  {
-    code: 1171,
-    day: "Heavy freezing drizzle",
-    night: "Heavy freezing drizzle",
-    icon: 284,
-  },
-  {
-    code: 1180,
-    day: "Patchy light rain",
-    night: "Patchy light rain",
-    icon: 293,
-  },
-  {
-    code: 1183,
-    day: "Light rain",
-    night: "Light rain",
-    icon: 296,
-  },
-  {
-    code: 1186,
-    day: "Moderate rain at times",
-    night: "Moderate rain at times",
-    icon: 299,
-  },
-  {
-    code: 1189,
-    day: "Moderate rain",
-    night: "Moderate rain",
-    icon: 302,
-  },
-  {
-    code: 1192,
-    day: "Heavy rain at times",
-    night: "Heavy rain at times",
-    icon: 305,
-  },
-  {
-    code: 1195,
-    day: "Heavy rain",
-    night: "Heavy rain",
-    icon: 308,
-  },
-  {
-    code: 1198,
-    day: "Light freezing rain",
-    night: "Light freezing rain",
-    icon: 311,
-  },
-  {
-    code: 1201,
-    day: "Moderate or heavy freezing rain",
-    night: "Moderate or heavy freezing rain",
-    icon: 314,
-  },
-  {
-    code: 1204,
-    day: "Light sleet",
-    night: "Light sleet",
-    icon: 317,
-  },
-  {
-    code: 1207,
-    day: "Moderate or heavy sleet",
-    night: "Moderate or heavy sleet",
-    icon: 320,
-  },
-  {
-    code: 1210,
-    day: "Patchy light snow",
-    night: "Patchy light snow",
-    icon: 323,
-  },
-  {
-    code: 1213,
-    day: "Light snow",
-    night: "Light snow",
-    icon: 326,
-  },
-  {
-    code: 1216,
-    day: "Patchy moderate snow",
-    night: "Patchy moderate snow",
-    icon: 329,
-  },
-  {
-    code: 1219,
-    day: "Moderate snow",
-    night: "Moderate snow",
-    icon: 332,
-  },
-  {
-    code: 1222,
-    day: "Patchy heavy snow",
-    night: "Patchy heavy snow",
-    icon: 335,
-  },
-  {
-    code: 1225,
-    day: "Heavy snow",
-    night: "Heavy snow",
-    icon: 338,
-  },
-  {
-    code: 1237,
-    day: "Ice pellets",
-    night: "Ice pellets",
-    icon: 350,
-  },
-  {
-    code: 1240,
-    day: "Light rain shower",
-    night: "Light rain shower",
-    icon: 353,
-  },
-  {
-    code: 1243,
-    day: "Moderate or heavy rain shower",
-    night: "Moderate or heavy rain shower",
-    icon: 356,
-  },
-  {
-    code: 1246,
-    day: "Torrential rain shower",
-    night: "Torrential rain shower",
-    icon: 359,
-  },
-  {
-    code: 1249,
-    day: "Light sleet showers",
-    night: "Light sleet showers",
-    icon: 362,
-  },
-  {
-    code: 1252,
-    day: "Moderate or heavy sleet showers",
-    night: "Moderate or heavy sleet showers",
-    icon: 365,
-  },
-  {
-    code: 1255,
-    day: "Light snow showers",
-    night: "Light snow showers",
-    icon: 368,
-  },
-  {
-    code: 1258,
-    day: "Moderate or heavy snow showers",
-    night: "Moderate or heavy snow showers",
-    icon: 371,
-  },
-  {
-    code: 1261,
-    day: "Light showers of ice pellets",
-    night: "Light showers of ice pellets",
-    icon: 374,
-  },
-  {
-    code: 1264,
-    day: "Moderate or heavy showers of ice pellets",
-    night: "Moderate or heavy showers of ice pellets",
-    icon: 377,
-  },
-  {
-    code: 1273,
-    day: "Patchy light rain with thunder",
-    night: "Patchy light rain with thunder",
-    icon: 386,
-  },
-  {
-    code: 1276,
-    day: "Moderate or heavy rain with thunder",
-    night: "Moderate or heavy rain with thunder",
-    icon: 389,
-  },
-  {
-    code: 1279,
-    day: "Patchy light snow with thunder",
-    night: "Patchy light snow with thunder",
-    icon: 392,
-  },
-  {
-    code: 1282,
-    day: "Moderate or heavy snow with thunder",
-    night: "Moderate or heavy snow with thunder",
-    icon: 395,
-  },
-];
-
-*/
